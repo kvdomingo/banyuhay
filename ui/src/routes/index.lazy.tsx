c58f338296f6b133ee932619ba02c0dfd7fc120d
Kenneth V. Domingo <hello@kvd.studio>
2024-10-05 03:41:22 +0800

    feat: render osm map

--- ui/src/routes/index.lazy.tsx
+++ ui/src/routes/index.lazy.tsx
@@ -1,9 +1,33 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import type { LatLngTuple } from "leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 export const Route = createLazyFileRoute("/")({
-  component: () => (
-    <div className="w-full h-dvh flex items-center justify-center text-5xl">
-      <h1>Banyuhay</h1>
-    </div>
-  ),
+  component: Index,
 });
+
+function Index() {
+  const initialZoom = 9;
+  const initialCenter: LatLngTuple = [15, 121];
+
+  return (
+    <div className="flex">
+      <div className="w-1/3 h-dvh px-8">
+        <h1 className="text-3xl my-8">Banyuhay</h1>
+        <p>Why?</p>
+      </div>
+
+      <MapContainer
+        center={initialCenter}
+        zoom={initialZoom}
+        scrollWheelZoom
+        className="w-full h-dvh"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+}
