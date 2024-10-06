973cef1bb9031d9329a17854c30a5ff0671f03a3
Kenneth V. Domingo <hello@kvd.studio>
2024-10-06 16:45:01 +0800

    build(ui): use single region

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -12,7 +12,7 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      regions: ["sin1", "hkg1", "syd1"],
+      regions: ["sin1"],
     }),
   },
 };
