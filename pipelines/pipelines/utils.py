d36140983d017664c738ab88d854710e6207b548
Kenneth V. Domingo <hello@kvd.studio>
2024-10-15 20:34:18 +0800

    fix(pipelines): establishment name splitting

--- pipelines/pipelines/utils.py
+++ pipelines/pipelines/utils.py
@@ -11,11 +11,11 @@ def get_rating(entry: list[str], attr: str) -> int:
 
 
 def get_caption(entry: list[str]):
-    end_idx = next(
+    prefix_end_idx = next(
         (i for i in range(len(entry)) if "water pressure" in entry[i].lower()),
         -1,
     )
-    start_idx = (
+    suffix_start_idx = (
         next(
             (i for i in range(len(entry)) if "poopability" in entry[i].lower()),
             -1,
@@ -24,8 +24,8 @@ def get_caption(entry: list[str]):
     )
     return "\n".join(
         [
-            "\n".join(e for e in entry[:end_idx] if not e.startswith("#")),
-            "\n".join(e for e in entry[start_idx:] if not e.startswith("#")),
+            "\n".join(e for e in entry[:prefix_end_idx] if not e.startswith("#")),
+            "\n".join(e for e in entry[suffix_start_idx:] if not e.startswith("#")),
         ]
     )
 
@@ -33,7 +33,7 @@ def get_caption(entry: list[str]):
 def get_establishment_name(entry: list[str]):
     splits = entry[0].lower().split(" sa ")
     if len(splits) > 1:
-        return splits[-1].split(".")[0]
+        return splits[1].split(".")[0]
     return None
 
 
