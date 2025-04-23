c299baf78e234d3e9425446ce2ed3b75a58a8f6d
Kenneth V. Domingo <hello@kvd.studio>
2025-04-24 02:33:04 +0800

    feat: toilet CRUD endpoints

--- api/app/routes/toilets.py
+++ api/app/routes/toilets.py
@@ -1,9 +1,15 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Security
 from pydantic import UUID4
 from starlette import status
+from starlette.responses import Response
 
+from app.auth import session_cookie_scheme
 from app.db.generated.models import Toilet
-from app.db.generated.toilets import AsyncQuerier
+from app.db.generated.toilets import (
+    AsyncQuerier,
+    CreateToiletParams,
+    UpdateToiletParams,
+)
 from app.db.queriers import get_toilet_async_querier
 
 router = APIRouter(prefix="/toilets", tags=["toilets"])
@@ -18,7 +24,41 @@ async def list_toilets(querier: AsyncQuerier = Depends(get_toilet_async_querier)
 async def get_toilet(
     id: UUID4, querier: AsyncQuerier = Depends(get_toilet_async_querier)
 ):
-    toilet = await querier.get_toilet(id=str(id))
-    if toilet is None:
+    if (toilet := await querier.get_toilet(id=str(id))) is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     return toilet
+
+
+@router.post("", response_model=Toilet, dependencies=[Security(session_cookie_scheme)])
+async def create_toilet(
+    body: CreateToiletParams, querier: AsyncQuerier = Depends(get_toilet_async_querier)
+):
+    if (toilet := await querier.create_toilet(body)) is None:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return toilet
+
+
+@router.patch(
+    "/{id}", response_model=Toilet, dependencies=[Security(session_cookie_scheme)]
+)
+async def update_toilet(
+    response: Response,
+    id: UUID4,
+    body: UpdateToiletParams,
+    querier: AsyncQuerier = Depends(get_toilet_async_querier),
+):
+    if (toilet := await querier.update_toilet(body)) is None:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    response.status_code = status.HTTP_202_ACCEPTED
+    return toilet
+
+
+@router.delete("/{id}", dependencies=[Security(session_cookie_scheme)])
+async def delete_toilet(
+    response: Response,
+    id: UUID4,
+    querier: AsyncQuerier = Depends(get_toilet_async_querier),
+):
+    if await querier.delete_toilet(id=id) is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    response.status_code = status.HTTP_204_NO_CONTENT
