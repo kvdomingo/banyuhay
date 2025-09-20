5929df6ce8b8b7c19b8adc79806b037b48aa24bc
Kenneth V. Domingo <hello@kvd.studio>
2025-09-20 20:22:54 +0800

    feat(ui) init react migration

---
+++ ui-next/src/components/header.tsx
@@ -0,0 +1,13 @@
+import { Link } from "@tanstack/react-router";
+
+export function Header() {
+  return (
+    <header className="p-2 flex gap-2 bg-white text-black justify-between">
+      <nav className="flex flex-row">
+        <div className="px-2 font-bold">
+          <Link to="/">Home</Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ ui-next/src/routes/__root.tsx
@@ -0,0 +1,38 @@
+import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanstackDevtools } from "@tanstack/react-devtools";
+
+import { Header } from "../components/header";
+
+import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
+
+import type { QueryClient } from "@tanstack/react-query";
+
+interface MyRouterContext {
+  queryClient: QueryClient;
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  component: Page,
+});
+
+function Page() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <TanstackDevtools
+        config={{
+          position: "bottom-left",
+        }}
+        plugins={[
+          {
+            name: "Tanstack Router",
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+          TanStackQueryDevtools,
+        ]}
+      />
+    </>
+  );
+}
